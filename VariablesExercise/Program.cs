97d3b44b9482ce07554f3fe2710f531408f77707
Pierre Dahrouj <pdahrouj7@gmail.com>
2024-09-18 09:29:33 -0700

    Uplaoding completed exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = "Pierre";
+            int age = 28;
+            char lastInitial = 'D';
+            bool genderMale = true;
+            double height = 6.00;
+            decimal dollarsInWallet = 20.50m;
+            string gender = genderMale ? "Male" : "Female";
+
+            Console.WriteLine($"Hello, my name is {name}, {lastInitial}. I am a {age} year old {gender}, {height} feet tall. I currently have {dollarsInWallet:C} in my wallet. ");
         }
     }
 }
